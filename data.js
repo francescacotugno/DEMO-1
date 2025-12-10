ec65c462e3a40f3728da82dc69fa9e7bf02b7a94
francescacotugno <francesca.cotugno@community.unipa.it>
2025-12-10 19:08:37 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,80 @@
+var fragData = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "id": "F01",
+        "luogo": "Treviri (Augusta Treverorum)",
+        "datazione": "II sec. d.C.",
+        "testo": "Deo VITIRIo L. Cassius Primus votum solvit",
+        "teonimo": "VITIRIVS",
+        "onomastica": "L. Cassius Primus"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [6.641, 49.756]  // [longitudine, latitudine]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": "F02",
+        "luogo": "Colonia Agrippinensis",
+        "datazione": "II–III sec. d.C.",
+        "testo": "MATRONIS AUFANIIS Secundina posuit",
+        "teonimo": "Matronae Aufaniae",
+        "onomastica": "Secundina"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [6.959, 50.938]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": "F03",
+        "luogo": "Bonna",
+        "datazione": "I sec. d.C.",
+        "testo": "Arminius filius Segomari annorum XX h.s.e.",
+        "teonimo": null,
+        "onomastica": "Arminius, Segomarus"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [7.098, 50.737]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": "F04",
+        "luogo": "Aquisgranum",
+        "datazione": "II sec. d.C.",
+        "testo": "DEO MERCURIO et ROSMERTAE Marcus Verus posuit",
+        "teonimo": "Mercurius, Rosmerta",
+        "onomastica": "Marcus Verus"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [6.083, 50.775]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": "F05",
+        "luogo": "Colonia Ulpia Traiana (Xanten)",
+        "datazione": "I–II sec. d.C.",
+        "testo": "DEO CAMVLO Severus Carantianus v.s.l.m.",
+        "teonimo": "Camulus",
+        "onomastica": "Severus Carantianus"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [6.452, 51.661]
+      }
+    }
+  ]
+};
